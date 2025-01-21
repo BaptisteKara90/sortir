a822e3a59ee6f9decae6a47c15aa2ae738164326
Baptiste Kara <baptiste.kara@gmail.com>
2025-01-21 16:53:25 +0100

    add list sortie + begining of filter search

---
+++ src/Form/FilterType.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Site;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FilterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('site', EntityType::class, [
+                'class' => Site::class,
+                'choice_label' => 'nom',
+            ])
+            ->add('content', TextType::class)
+            ->add('dateDebut', DateTimeType::class, [
+                'widget' => 'single_text',
+            ])
+            ->add('dateFin', DateTimeType::class, [
+                'widget' => 'single_text',
+            ])
+            ->add('organisateur', CheckboxType::class, [
+                'required' => false,
+                'label' => "Sorties dont je suis l'organisateur/trice",
+            ])
+            ->add('inscrit', choiceType::class, [
+                'required' => false,
+                'label' => 'Inscrit',
+                'choices' => [
+                    'Oui' => 'inscrit',
+                    'Non' => 'non inscrit',
+                    'Tout' => 'tout',
+                ]
+            ])
+            ->add('sortiePassee', CheckboxType::class, [
+                'required' => false,
+                'label' => 'Sorties Passées',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => null,
+        ]);
+    }
+}
--- src/Repository/SortieRepository.php
+++ src/Repository/SortieRepository.php
@@ -16,28 +16,10 @@ class SortieRepository extends ServiceEntityRepository
         parent::__construct($registry, Sortie::class);
     }
 
-    //    /**
-    //     * @return Sortie[] Returns an array of Sortie objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('s')
-    //            ->andWhere('s.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('s.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Sortie
-    //    {
-    //        return $this->createQueryBuilder('s')
-    //            ->andWhere('s.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+    public function findBySite($site){
+       $qb = $this->createQueryBuilder('s');
+       $qb->where('s.site = :site')->setParameter('site', $site);
+        $query = $qb->getQuery();
+        return $query->getResult();
+    }
 }
